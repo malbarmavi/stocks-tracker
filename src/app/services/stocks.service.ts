7aebad18c901cba71a9e6444167f6133ae3a8250
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-05-18 09:47:09 +0300

    Update stocks service

--- src/app/services/stocks.service.ts
+++ src/app/services/stocks.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class StocksService {
+    private stocks: Array<string> = ['MSFT', 'GOOG', 'FB', 'TWTR'];
+    private http: Http;
+    private stocksUrl = "http://finance.google.com/finance/info?client=ig&q=NASDAQ%";
 
-  constructor() { }
+    constructor(http: Http) {
+        this.http = http;
+    }
 
+    public get() {
+        return this.stocks.slice(0);
+    }
+
+    public add(stock) {
+        this.stocks.push(stock);
+        return this.get();
+    }
+
+    public load() {
+        return this.http.get(`${this.stocksUrl}${this.stocks.join(',')}`);
+    }
 }
