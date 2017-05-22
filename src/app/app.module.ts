827b37bdb83e2fdc40fe748b6947cf3ea6537dad
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-05-22 04:54:53 +0300

    Add stock component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StockComponent } from './components/stock/stock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    StockComponent
   ],
   imports: [
     BrowserModule,
