c20c50d5df0eda18aa5d48ddf97d27417ab0e4ef
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-05-23 23:40:59 +0300

    Implement stocks manage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -19,7 +19,8 @@ import { ManageComponent } from './components/manage/manage.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
--- src/app/components/manage/manage.component.ts
+++ src/app/components/manage/manage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StocksService } from '../../services/stocks.service';
+
 
 @Component({
   selector: 'app-manage',
@@ -6,8 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage.component.css']
 })
 export class ManageComponent implements OnInit {
+  private service: StocksService;
+  private stocksForm: FormGroup;
+  private stocksKeys: string[];
 
-  constructor() { }
+  constructor(stocksService: StocksService, formBuilder: FormBuilder) {
+    this.service = stocksService;
+    this.stocksForm = formBuilder.group({
+      'newStock': ['', Validators.required]
+    });
+    this.stocksKeys = this.service.get();
+  }
+
+  public add(stock: string): void {
+    if (stock && stock.trim().length > 0) {
+      this.stocksKeys = this.service.add(stock);
+    }
+    this.stocksForm.controls['newStock'].setValue('');
+  }
+
+  public remove(stock): void {
+    this.stocksKeys = this.service.remove(stock);
+  }
 
   ngOnInit() {
   }
